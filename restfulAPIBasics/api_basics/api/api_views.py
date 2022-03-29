449ede9612ebb30ee40b627c315dcc8e3842b4ee
tanjimash <tanjimashraf.cit.bd@gmail.com>
2022-03-29 15:48:57 +0600

    initial commit

---
+++ restfulAPIBasics/api_basics/api/api_views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from ..models import Article
+
+from rest_framework.decorators import api_view  # rest_framework decorator
+from rest_framework.response import Response    # throw the response in the DRF UI
+from ..serializers import ArticleSerializer     # import the serializer
+from rest_framework import status
+# from django.views.decorators.csrf import csrf_exempt  # not required while using the 'POST' api_view
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'API Overview': 'api/',
+        'List': 'api/article-list/',
+        'Detail View': 'article/<str:pk>',
+        'Create': 'article-create/',
+        'Update': 'article-update/<str:pk>/',
+        'Delete-specific': 'article-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def article_list(request):
+    articles = Article.objects.order_by('-id')
+    # serialize the queryset, along with the param 'many=True', cause there will be multiple data
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def article_detail(request, pk):
+    article = Article.objects.get(id=pk)
+    serializer = ArticleSerializer(article, many=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def article_create(request):
+    serializer = ArticleSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def article_update(request, pk):
+    article = Article.objects.get(id=pk)
+    serializer = ArticleSerializer(instance=article, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['DELETE'])
+def article_delete(request, pk):
+    article = Article.objects.get(id=pk)
+    article.delete()
+
+    # after deletion, return the new article-list
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
---
+++ restfulAPIBasics/api_basics/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Article
+
+
+# # Serializer class for 'Article'
+# class ArticleSerializers(serializers.Serializer):
+#     # same fields from the model 'Article'
+#     title = serializers.CharField(max_length=100)
+#     author = serializers.CharField(max_length=100)
+#     email = serializers.EmailField(max_length=100)
+#     date = serializers.DateField(auto_now_add=True)
+#
+#     # create func
+#     def create(self, validated_data):
+#         return Article.objects.create(validated_data)
+#
+#     # update func
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.email = validated_data.get('email', instance.email)
+#         instance.date = validated_data.get('date', instance.date)
+#         instance.save()
+#         # lastly, save() the instance
+#         return instance
+
+
+# Model Serializer class for model 'Article'
+# It's more like the django's form
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'author', 'email', 'date']
+        # fields = '__all__'
+
+
