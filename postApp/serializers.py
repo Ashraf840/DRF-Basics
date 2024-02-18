1809171748b8f32ed33c102f59745bec0372d603
Ashraf840 <shift2unleashed2011@gmail.com>
2024-02-19 00:21:02 +0600

    Make a serializer manually without using ModelSerializer library for 'Post' class-model

---
+++ postApp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class PostSerializer(serializers.Serializer):
+    id=serializers.IntegerField(read_only=True)
+    title=serializers.CharField(max_length=60)
+    content=serializers.CharField()
+    created_at=serializers.DateTimeField(read_only=True)
