89b26842a4fb38b7181c20d5fce159986d0c264a
Ashraf840 <shift2unleashed2011@gmail.com>
2024-02-20 00:54:58 +0600

    Started adding validations into model-serializer

--- postApp/serializers.py
+++ postApp/serializers.py
@@ -10,6 +10,8 @@ class PostSerializer(serializers.Serializer):
     created_at=serializers.DateTimeField(read_only=True)
 
 class PostModelSerializer(serializers.ModelSerializer):
+    # Add validations for creating or updating posts through this serializer
+    title=serializers.CharField(max_length=50)  # Ensures the char limit to 50 while creating/updating any post using this serializer
     class Meta:
         model=Post
         fields=['id','title','content','created_at']
