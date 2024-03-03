dc061368f5d43e9aa07f790833050f0b22f1bd60
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-03 21:27:46 +0600

    Added default pagination class in settings.py file

--- blogSystem/settings.py
+++ blogSystem/settings.py
@@ -66,6 +66,8 @@ WSGI_APPLICATION = "blogSystem.wsgi.application"
 
 REST_FRAMEWORK = {
     "NON_FIELD_ERRORS_KEY":"errors",
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     "DEFAULT_AUTHENTICATION_CLASSES":(
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
